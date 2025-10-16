657b779874aff354b432d30d73e2cb936ced5e27
Bodhisatva Tiwari <156921822+tiwaribodhi@users.noreply.github.com>
2025-10-16 23:22:51 +0530

    Update matplotlib_project_2.py

--- matplotlib_project_2.py
+++ matplotlib_project_2.py
@@ -1,159 +1,124 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv(r"C:\Users\DELL\Downloads\Liverpool.csv")
+df = pd.read_csv(r"C:\New folder\Program1\Main\Python Library\Employment_Unemployment_GDP_data.csv")
 
-"""Ques. Home vs Away Performance Gap
+""" Ques. Global Trend Over Time (Lineplot)
+ How has the global unemployment rate changed over the years?
 
-Problem:
-Does Liverpool perform significantly better at home than away?
-
-Create a side-by-side bar chart comparing average Goals scored and average Goals conceded in Home vs Away matches.
-
-Add exact values above each bar. **bold text**
 """
 
-home_goals = df[df['Is_Home'] == 1]['Goals'].mean()
-home_conceded = df[df['Is_Home'] == 1]['Opponent_Goals'].mean()
+sns.relplot(data=df, x="Year", y="Unemployment Rate", errorbar="ci",  kind="line")
 
-away_goals = df[df['Is_Home'] == 0]['Goals'].mean()
-away_conceded = df[df['Is_Home'] == 0]['Opponent_Goals'].mean()
+"""Ques.  Sector Distribution
+In the most recent year, what’s the employment split (Agri/Industry/Services) across top 5 economies? **bold text**
 
-labels = ['Home', 'Away']
-goals_scored = [home_goals, away_goals]
-goals_conceded = [home_conceded, away_conceded]
+"""
 
-import matplotlib.pyplot as plt
-import numpy as np
+top5 = df.nlargest(5, "Year")
 
-x = np.arange(len(labels))  # [0, 1] → positions for Home, Away
-width = 0.35  # bar width
+top5_melted = top5.melt(
+    id_vars="Country Name",
+    value_vars=['Employment Sector: Agriculture', 'Employment Sector: Industry', 'Employment Sector: Services'],
+    var_name="Sector",
+    value_name="Employment"
+)
+sns.catplot(
+    data=top5_melted,
+    x="Country Name",
+    y="Employment",
+    hue="Sector",
+    kind="bar"
+)
 
-fig, ax = plt.subplots(figsize=(8, 6))
-bars1 = ax.bar(x - width/2, goals_scored, width, label='Goals Scored', color='green')
-bars2 = ax.bar(x + width/2, goals_conceded, width, label='Goals Conceded', color='red')
-
-# Add labels, title, ticks
-ax.set_ylabel('Average Goals')
-ax.set_title('Liverpool Home vs Away Performance')
-ax.set_xticks(x)
-ax.set_xticklabels(labels)
-ax.legend()
-
-for bars in [bars1, bars2]:
-    for bar in bars:
-        height = bar.get_height()
-        ax.annotate(f'{height:.2f}',
-                    xy=(bar.get_x() + bar.get_width()/2, height),
-                    xytext=(0, 3),  # 3 points above bar
-                    textcoords="offset points",
-                    ha='center', va='bottom')
-
-plt.tight_layout()
 plt.show()
 
-"""Ques. Match Outcome Breakdown
-
-Problem:
-The club’s management wants a clear view of how often matches end in Win, Draw, or Loss.
-
-Create a pie chart showing the distribution of results (Result column).
-
-Use percentages and labels.
-
-Add a title: “Liverpool Match Outcomes”.
+"""Ques. Relationship
+Is there a correlation between GDP and Unemployment Rate? **bold text**
 """
 
-h1 = df[df['Result'] == 1]['Result'].value_counts().reset_index()
-h2 = df[df['Result'] == 0]['Result'].value_counts().reset_index()
-h3 = df[df['Result'] == -1]['Result'].value_counts().reset_index()
-
-g = [h1.iloc[0, 1], h2.iloc[0, 1], h3.iloc[0, 1]]
-label = ['Win', 'Draw', 'Loss']
-
-plt.pie(g, labels=label, autopct='%1.1f%%')
-plt.title('Liverpool Match Outcomes')
+sns.regplot(data=df, x="Unemployment Rate", y="GDP (in USD)", scatter=True, fit_reg=True)
+df["GDP (in USD)"].corr(df["Unemployment Rate"])
 plt.show()
 
-"""Ques. Shot Efficiency vs Goals
-
-Problem:
-Is higher shot efficiency actually leading to more goals?
-
-Plot a scatter plot with Shot_Efficiency on the x-axis and Goals on the y-axis.
-
-Add a trend line to show the overall relationship. **bold text**
-
+"""Ques. Country Philosophy
+ Do countries show different employment strategies?
+E.g., Are some still agriculture-heavy vs services-heavy? **bold text**
 """
 
-x = df['Shot_Efficiency']
-y = df['Goals']
+df_sorted = df.sort_values("GDP (in USD)", ascending=False)
+unique_top = df_sorted.drop_duplicates("Country Name").head(10)
 
-x = x.replace([np.inf, -np.inf], np.nan)
-y = y.replace([np.inf, -np.inf], np.nan)
-mask = ~(x.isna() | y.isna())
-x, y = x[mask], y[mask]
+#print("\nBest row per category:")
+#print(unique_top)
 
-plt.scatter(x, y, alpha=0.5)
+top5_melted = unique_top.melt(
+    id_vars="Country Name",
+    value_vars=['Employment Sector: Agriculture', 'Employment Sector: Industry', 'Employment Sector: Services'],
+    var_name="Sector",
+    value_name="Employment"
+)
+sns.catplot(
+    data=top5_melted,
+    x="Country Name",
+    y="Employment",
+    hue="Sector",
+    kind="bar",
+    height=5,     # height of the chart
+    aspect=3
+)
 
-m, b = np.polyfit(x, y, 1)
-plt.plot(x, m*x + b, color='red', label='Trendline')
-
-plt.xlabel("Shot Efficiency")
-plt.ylabel("Goals")
-plt.title("Shot Efficiency vs Goals")
-plt.legend()
+plt.xticks(rotation=45)
 plt.show()
 
-"""Ques. Opponent Strength Effect
-
-Problem:
-Do Liverpool struggle more against strong teams (high possession opponents)?
-
-Create a line plot of Opponent_Possession vs Goals scored by Liverpool.
-
-Group by Opponent (take average values per opponent) and sort by possession.
-
-Highlight top 3 toughest opponents in red markers. **bold text**
+"""Ques. Change Over Time
+ For a specific country (say India, USA, Germany), how did sector employment shift from 1991–latest year?
 """
 
-df_grouped = df.groupby("Opponent").agg({
-    "Opponent_Possession": "mean",
-    "Goals": "mean"
-}).reset_index()
+ho = df[df['Country Name'] == 'India'][['Year', 'Employment Sector: Agriculture', 'Employment Sector: Industry', 'Employment Sector: Services']]
 
-df_grouped = df_grouped.sort_values(by="Opponent_Possession")
+top5_melted = ho.melt(
+    id_vars="Year",
+    value_vars=['Employment Sector: Agriculture', 'Employment Sector: Industry', 'Employment Sector: Services'],
+    var_name="Sector",
+    value_name="Employment"
+)
 
-plt.figure(figsize=(16,10))
+sns.relplot(
+    data=top5_melted,
+    x="Year",
+    y="Employment",
+    hue="Sector",
+    kind="line",
+    marker="o",
+    height=5,
+    aspect=3
+)
 
-plt.plot(df_grouped["Opponent_Possession"], df_grouped["Goals"], marker='o')
-top3 = df_grouped.nlargest(3, "Opponent_Possession")
-plt.scatter(top3["Opponent_Possession"], top3["Goals"], color="red", s=100, label="Top 3")
-
-plt.xlabel("Opponent Possession (%)")
-plt.ylabel("Avg Goals by Liverpool")
-plt.title("Opponent Strength Effect on Goals")
-plt.legend()
 plt.show()
 
-"""Ques. Last 5 Match Momentum
-
-Problem:
-Does recent momentum (Last5_Win_Rate) correlate with match outcome?
-
-Create a box plot of Last5_Win_Rate split by Result (Win/Draw/Loss).
-
-Check if higher momentum → more wins. **bold text** *italicized text*
+"""Ques. Multi-variable Comparison (Pairplot)
+ Compare GDP, Unemployment, and 3 employment sectors simultaneously. **bold text**
 """
 
-h1 = df[df['Result'] == 1]['Last5_Win_Rate']
-h2 = df[df['Result'] == 0]['Last5_Win_Rate']
-h3 = df[df['Result'] == -1]['Last5_Win_Rate']
+cols = ['GDP (in USD)', 'Employment Sector: Agriculture', 'Employment Sector: Industry', 'Employment Sector: Services', 'Unemployment Rate']
 
-g = [h1, h2,h3]
+sns.pairplot(df[cols], diag_kind="kde", corner=True)
+plt.show()
 
-plt.figure(figsize=(8,6))
-plt.boxplot(g, labels=['Win', 'Draw', 'Loss'])
-plt.title('Liverpool Match Outcomes')
+"""Ques. Heatmap (Final Summary)
+ Which countries have the highest unemployment vs GDP across years?
+"""
+
+p = pd.pivot_table(
+    df,
+    values="Unemployment Rate",
+    index="Country Name",
+    columns="Year",
+    aggfunc="mean"
+)
+
+p_small = p.loc[p.index[:10]]
+sns.heatmap(p_small, annot=True, cmap="YlGnBu", linewidths=1)
 plt.show()
